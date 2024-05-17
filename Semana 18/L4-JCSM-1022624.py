f8e1a2ac660060d0b9b5333b6a7f9d33b15fb2f5
josesauced <157129737+josesauced@users.noreply.github.com>
2024-05-17 08:18:33 -0600

    Add files via upload

---
+++ Semana 18/L4-JCSM-1022624.py
@@ -0,0 +1,38 @@
+#Tema 1 
+
+# def obtener_mayor(precios):
+#     mayor = precios[0]
+#     for precio in precios:
+#         if precio > mayor:
+#             mayor = precio
+#     return mayor
+
+# def obtener_menor(precios):
+#     menor = precios[0]
+#     for precio in precios:
+#         if precio < menor:
+#             menor = precio
+#     return menor
+
+# precios = [50, 75, 46, 22, 80, 65, 8]
+
+# mayor_precio = obtener_mayor(precios)
+# menor_precio = obtener_menor(precios)
+
+# print("El mayor precio es:", mayor_precio)
+# print("El menor precio es:", menor_precio)
+
+
+# #Tema 2 
+
+# abecedario = list('ABCDEFGHIJKLMNÑOPKRSTUVWXYZ')
+# del abecedario[2::3]
+# print(abecedario)
+
+
+# # Tema 3 
+
+# palabras = ("Casa","Comida","Sala","Carro","Cocina", "Cama","Baño","Colegio") 
+# palabra_a_buscar = input("Ingrese una palabra: ")
+# conteo = palabras.count(palabra_a_buscar)
+# print("La palabra '{}' aparece {} veces en la lista.".format(palabra_a_buscar, conteo))
